236a939f51893fe5ec12f1ce904bda707fbdfa7e
Sander <santoe_vankoppen@hotmail.com>
2025-04-02 12:06:03 +0000

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,16 +1,22 @@
-import { Box, AppBar, Toolbar, Typography, Container, MenuItem } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Container, MenuItem, LinearProgress } from "@mui/material";
 import { Group } from "@mui/icons-material";
 import { NavLink } from "react-router";
 import MenutItemLink from "../shared/components/MenutItemLink";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 
 export default function NavBar() {
+    const { uiStore } = useStore();
+
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{
-                backgroundImage: 'linear-gradient(123deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-
-            }}>
+            <AppBar
+                position="static"
+                sx={{
+                    backgroundImage: 'linear-gradient(123deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+                    position: 'relative'
+                }}>
                 <Container maxWidth='xl'>
                     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Box>
@@ -26,12 +32,28 @@ export default function NavBar() {
                             <MenutItemLink to='/createActivity'>
                                 Create activity
                             </MenutItemLink>
+                            <MenutItemLink to='/counter'>
+                                Counter
+                            </MenutItemLink>
                         </Box>
                         <MenuItem>
                             User menu
                         </MenuItem>
                     </Toolbar>
                 </Container>
+                <Observer>
+                    {() => uiStore.isloading ? (
+                        <LinearProgress
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                height: 4
+                            }}
+                            color="secondary" />
+                    ) : null}
+                </Observer>
             </AppBar>
         </Box>
     );
